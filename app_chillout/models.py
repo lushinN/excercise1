87ba410776b05626565a6e70de68770684860c8c
Pablo <loshakov.07p@gmail.com>
2023-08-16 15:25:49 +0300

    first commit

--- app_chillout/models.py
+++ app_chillout/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib import admin
 from  django.utils import timezone
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
 
@@ -11,6 +14,10 @@ class Advertisement(models.Model):
     acution = models.BooleanField('Торг', help_text='Укажите, уместен ли торг')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='пользователь',
+                             on_delete=models.CASCADE)
+
+    image = models.ImageField('Изображение', upload_to='chillout/')
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
@@ -19,9 +26,19 @@ class Advertisement(models.Model):
     def created_date(self):
         if self.created_at.date == timezone.now().date():
             created_time = self.created_at.time().strftime('%H:%M:%S')
-            return 'Сегодня в ' + str(created_time)
+            return  ('<span style = "color: purple; font-weight: bold"> Сегодня в {}</span>', created_time
+                     )
         else:
             return self.created_at.strftime('%d.%m.%y' ' в %H:%M:%S')
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        if self.updated_at.date == timezone.now().date():
+            updated_time = self.updated_at.time().strftime('%H:%M:%S')
+            return ('<span style = "color: orange; font-weight: bold"> Сегодня в {}</span>', updated_time
+                     )
+        else:
+            return self.updated_at.strftime('%d.%m.%y' ' в %H:%M:%S')
 
 
 
--- chillout/settings.py
+++ chillout/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
@@ -125,7 +125,15 @@ STATICFILES_DIRS = [
     Path.joinpath(BASE_DIR, 'static')
 ]
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOCALE_PATHS = (
+    Path.joinpath(BASE_DIR, 'apps'),
+)
